499930212a09b45cdad4e5c7cdbe715ccf86e48c
AliLynne <alilynne.thompson@gmail.com>
2019-03-28 15:34:22 -0700

    Finished adding questions

--- day.js
+++ day.js
@@ -44,7 +44,106 @@ const questions = [
     message: 'Were you more sensitive to rejection or were your feelings more easily hurt today?'
   },
   {
-    
+    type: 'confirm',
+    name: 'q7',
+    message: 'Did you feel angry or irritable today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q8',
+    message: 'Did you have any conflicts or problems with people today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q9',
+    message: 'Did you have less interest in your usual activities? (School, work, friends, hobbies)'
+  },
+  {
+    type: 'confirm',
+    name: 'q10',
+    message: 'Did you have difficulty concentrating today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q11',
+    message: 'Did you feel tired, fatigued or have a lack of energy today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q12',
+    message: 'Was your appetite increased or did you overeat?'
+  },
+  {
+    type: 'confirm',
+    name: 'q13',
+    message: 'Did you have cravings for any specific foods?'
+  },
+  {
+    type: 'confirm',
+    name: 'q14',
+    message: 'Did you sleep more, take naps, or have difficulty getting up when intended today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q15',
+    message: 'Did you have trouble getting to sleep or staying asleep last night?'
+  },
+  {
+    type: 'confirm',
+    name: 'q16',
+    message: 'Did you feel overwhelmed or that you could not cope today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q17',
+    message: 'Did you feel out of control today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q18',
+    message: 'Were your breasts tender today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q19',
+    message: 'Did you have breats swelling, feel bloated, or have any weight gain today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q20',
+    message: 'Did you have a headache today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q21',
+    message: 'Did you have joint or muscle pain today?'
+  },
+  {
+    type: 'confirm',
+    name: 'q22',
+    message: 'At work, school, home, or in daily routine, did at least one of the previous problems cause reduced productivity or inefficiency?'
+  },
+  {
+    type: 'confirm',
+    name: 'q23',
+    message: 'Did at least one of the previous problems interfere with your hobbies or social activities? i.e. Did you avoid any of them?'
+  },
+  {
+    type: 'confirm',
+    name: 'q24',
+    message: 'Did at least one of the previous problems interfere with your relationships with others?'
+  },
+  {
+    type: 'list',
+    name: 'menstrualFlow',
+    message: 'What was the rate of your menstrual flow today?',
+    choices: [
+      'Heavy',
+      'Medium',
+      'Light',
+      'Spotting',
+      'None'
+    ]
   }
 ]
 
